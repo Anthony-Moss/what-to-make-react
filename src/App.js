b6f538d46a2abf9e816fed396b21838cb2bdb4e4
Anthony Moss <asmoss93@gmail.com>
2019-10-07 19:09:46 -0400

    adds navbar and some text for the home page of site/app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import FindRecipe from './components/FindRecipe';
+import NavBar from './components/NavBar';
 
 function App() {
+  let navLinks = ['Home', 'Search', 'Random']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <FindRecipe />
+    <div className="Home-container">
+      <header className="Home-header">
+        <h1>What To Make!</h1>
+      
+        <NavBar data={navLinks}/>
+        {/* <FindRecipe /> */}
+      
       </header>
+      <div>
+        <h2>Welcome to What To Make</h2>
+        <p>The website to help you decide what to cook!</p>
+        <h3>Stop Thinking Start Cooking!</h3>
+      </div>
     </div>
   );
 }
