470ff3b0718a2c44cb44b4c33787815a8c30265b
Anthony Moss <asmoss93@gmail.com>
2019-10-08 23:37:22 -0400

    adds start of home page component, for apps main landing page

---
+++ src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Welcome to What To Make</h2>
+                <p>The website to help you decide what to cook!</p>
+                <h3>Stop Thinking Start Cooking!</h3>
+            </div>
+        )
+    }
+}
+
+export default HomePage
