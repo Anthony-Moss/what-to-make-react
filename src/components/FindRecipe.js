42f6a41b6863b0a1fbeec95f128262e692317af3
Anthony Moss <asmoss93@gmail.com>
2019-10-10 17:30:41 -0400

    fixes the search endpoint, cleans up code for read ability, adds function that gets recipe info by id

--- src/components/FindRecipe.js
+++ src/components/FindRecipe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RecipeCard from './RecipeCard';
 const Axios = require('axios');
 
 class FindRecipe extends React.Component {
@@ -7,21 +8,26 @@ class FindRecipe extends React.Component {
 
         this.state = {
             criteria: [],
-            recipes: []
+            recipeCards: []
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
     render() {
+        let cards = this.state.recipeCards.map((recipe, i) => {
+            return <RecipeCard key={recipe.id} id={recipe.id} title={recipe.title} image={recipe.image} />
+        })
         return (
-            <div>
+            <div className='search-container'>
                 <h1>Search recipes</h1>
+                <div className='search-bar'>
 
-                <input type='text' name="criteria" placeholder="Search by Ingredients" value={this.state.criteria} onChange={this.handleChange}></input>
-                <input type="submit" value="Search" onClick={this.search}/>
-
-                <p>{this.state.recipes[0]}</p>
-                <p>{this.state.recipes[2]}</p>
+                    <input type='text' name="criteria" placeholder="Search by Ingredients" value={this.state.criteria} onChange={this.handleChange}></input>
+                    <input type="submit" value="Search" onClick={this.searchComplex}/>
+                </div>
+                <div className='recipe-cards'>
+                    {cards}
+                </div>
             </div>
         )
     }
@@ -29,30 +35,56 @@ class FindRecipe extends React.Component {
     handleChange = (event) => {
         this.setState({[event.target.name]: [event.target.value]});
     }
-
-    search = async () => {
-        const recipes = localStorage.getItem('currentRecipes')
+    // https://api.spoonacular.com/recipes/complexSearch
+    searchComplex = async () => {
+        // const recipes = localStorage.getItem('lastSearchedRecipes')
         // console.log(recipes.length)
-        if (recipes === null || recipes.length === 0) {
+        // if (recipes === null || recipes.length === 0) {
+
+        // make get request to recipe api to get info
             try {
                 const response = await Axios({
                     method: 'GET',
-                    url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.REACT_APP_SPOON_API_KEY}&&ingredients=${this.state.criteria[0]}`
+                    // spoontaculars complex search endpoint to get details to make recipe cards (id, name, image)
+                    url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOON_API_KEY}&includeIngredients=${this.state.criteria[0]}&instructionsRequired=True`
                 })
-                const recipeJSON = JSON.stringify(response.data)
-                localStorage.setItem('currentRecipes', localStorage.getItem('currentRecipes') + recipeJSON)
-                this.setState({"recipes": [response.data[0].title, response.data[0].id, response.data[0].instructions, response.data[0].image]})
+                // console.log(response)
+                // const recipeJSON = JSON.stringify(response.data)
+                // localStorage.setItem('lastSearchedRecipes', localStorage.getItem('lastSearchedRecipes') + recipeJSON)
+
+                // Map through 
+                let cardInfo = response.data.results
+                // sanity check for info is correct
+                console.log(cardInfo)
+                // Sets the state with the recipe data needed for cards
+                this.setState({"recipeCards": cardInfo})
             } catch (error) {
                 alert(error)
                 console.log(error);
             }
-        }
+        // }
         
         // const recipeData = JSON.parse(recipes)
         // console.log(recipeData)
-        // console.log(localStorage.getItem('currentRecipes'))
+        // console.log(localStorage.getItem('lastSearchedRecipes'))
         
     }
+
+    getBulkRecipes = async () => {
+        let ids = []
+        this.state.recipes.map((recipe) => {
+            ids.push(recipe[1])
+        })
+        try {
+            const response = await Axios({
+                method: 'GET',
+                url: `https://api.spoonacular.com/recipes/informationBulk?apiKey=${process.env.REACT_APP_SPOON_API_KEY}&ids=${this.state.recipes.id}`
+            })
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }  
 }
 
 export default FindRecipe
