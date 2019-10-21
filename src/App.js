447ceec24ba131766159168d625562862b96a766
Anthony Moss <asmoss93@gmail.com>
2019-10-21 18:02:42 -0400

    brings in new component RecipeBook that will show different saved recipes, given recipe info is given as props taken from local storage for now

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import RandomRecipe from './components/RandomRecipe';
 import SimpleRecipeSearch from './components/SimpleRecipeSearch';
+import RecipeBook from './components/RecipeBook';
 
 function App() {
-  let navLinks = ['Home', 'Search', 'Random']
+  let favorites = localStorage.getItem('favoriteRecipes')
+  let toMake = localStorage.getItem('toMakeRecipes')
+  let allRecipes = localStorage.getItem('allRecipes')
 
   return (
     <div className="Home-container">
@@ -21,6 +24,7 @@ function App() {
       <FindRecipe />
       {/* <SimpleRecipeSearch /> */}
       <RandomRecipe /> 
+      <RecipeBook favorites={favorites} toMake={toMake} allRecipes={allRecipes} />
     </div>
   );
 }
